bac8b277ce43ad5b40afcc3939b2a7bd264ab015
GiovannaNaves <110492330+GiovannaNaves@users.noreply.github.com>
2023-03-28 15:46:48 -0300

    updates

--- Aula2Fat/TP/src/fib.cpp
+++ Aula2Fat/TP/src/fib.cpp
@@ -1,3 +1,23 @@
 #include "fib.h"
 
 using namespace std;
+
+int fibr(int n) { 
+    if (n < 3) 
+        return 1; 
+    else 
+        return Fib(n-1) + Fib(n-2);
+}
+
+int fibi(int n) { 
+    int fn1 = 1, fn2 = 1; 
+    int fn, i; 
+    if (n < 3) 
+        return 1; 
+    for (i = 3; i <= n; i++) { 
+        fn = fn2 + fn1; 
+        fn2 = fn1; 
+        fn1 = fn; 
+    } 
+    return fn;
+}
--- Aula2Fat/TP/src/fibop.cpp
+++ Aula2Fat/TP/src/fibop.cpp
@@ -1,3 +1,10 @@
 #include "fib.h"
 
 using namespace std;
+
+int main() { 
+    int f; 
+    f = fibr(4);
+    cout << f << endl; 
+     
+}
