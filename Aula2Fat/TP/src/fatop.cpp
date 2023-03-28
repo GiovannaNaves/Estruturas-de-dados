cfbc941f1a3089dff166ac4d38ce5d97b4487579
GiovannaNaves <110492330+GiovannaNaves@users.noreply.github.com>
2023-03-28 16:30:55 -0300

    Update fatop.cpp

--- Aula2Fat/TP/src/fatop.cpp
+++ Aula2Fat/TP/src/fatop.cpp
@@ -4,7 +4,12 @@ using namespace std;
 
 
 int main( int argc, char ** argv) { 
-    int f = atoi(argv[1]); 
-    cout << fatr(f) << endl; 
+    struct timespec t1, t2, res;
+    int n = atoi(argv[1]); 
+    cout << argc << argv[0] << argv[1] << endl;
+    clock_gettime(CLOCK_MONOTONIC,&t1);
+    cout << fatr(n) << endl; 
+    clock_gettime(CLOCK_MONOTONIC,&t2);
+    if ()
      
 }
