4a86d3cb2f4bbcbdcda10edffde43938ac830901
GiovannaNaves <110492330+GiovannaNaves@users.noreply.github.com>
2023-04-04 16:31:40 -0300

    .

--- Aula3/c3correto.c
+++ Aula3/c3correto.c
@@ -5,9 +5,7 @@ int main(int argc, char** argv){
   int i;
   int *a;
 
-  for (i=0; i < 10; i++){
-    a = malloc(sizeof(int) * 100);
-  }
+    a = malloc(sizeof(int) * 1000);
   free(a);
   return 0;
 }
