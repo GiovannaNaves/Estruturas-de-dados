da12fcec3b8746bd7a44089ce21d3ac6f5931101
GiovannaNaves <110492330+GiovannaNaves@users.noreply.github.com>
2023-04-06 22:40:35 -0300

    Update c10correto.c

--- Aula3/c10correto.c
+++ Aula3/c10correto.c
@@ -8,19 +8,12 @@ int main(void)
 {
         int *p;
         int i = 0;
-
         while(i<MAX_ITERATIONS)
         {
                 p = malloc(128);
-                if (p == NULL) {
-            printf("Failed to allocate memory!\n");
-            break;
+                printf("%ld\n", (long)p);       
+                free(p);
+                i++;
         }
-        printf("%ld\n", (long)p);       
-        free(p);
-        i++;
-
-        }
-        
         return (0);
 }
