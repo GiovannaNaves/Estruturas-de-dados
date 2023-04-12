5c4423bf514c965a7df214814defe612250c5c43
GiovannaNaves <110492330+GiovannaNaves@users.noreply.github.com>
2023-04-12 11:36:36 -0300

    ;

---
+++ Aula4/TP/src/main.cpp
@@ -0,0 +1,19 @@
+#include "pilha.hpp"
+#include <ostdlib>
+#include <ctime>
+
+using namespace std;
+
+int main(){
+    Pilha p;
+    srand(time(NULL));
+    for (int i = 1; i <=10; i++){
+        int num = rand() % 10;
+        cout << num << endl;
+        p.Empilhar(num); 
+    }
+    for (int i = 1; i <=10; i++){
+        cout << p.Desempilhar() << endl;
+    }
+    return 0;
+}
---
+++ Aula4/TP/src/pilha.cpp
@@ -0,0 +1,48 @@
+#include "fila.hpp"
+  
+Pilha::Pilha(){
+    topo = NULL;
+    tamanho = 0;
+}
+
+void Pilha::PilhaVazia(){
+    topo = NULL;
+    tamanho = 0;
+}
+
+void Pilha::Empilhar(int p){
+    TipoCelula<int> *nova = new TipoCelula<int>();
+    nova->item = p;
+    nova->prox = topo;
+    topo = nova;
+    tamanho++;
+}
+
+int Pilha::Desempilhar(){
+    if(tamanho == 0) throw "Pilha vazia!";
+    TipoCelula<int> *p = topo;
+    int aux = topo->item;
+    topo = topo->prox;
+    delete p;
+    tamanho--;
+    return aux;
+}
+
+int Pilha::GetTopo(){
+    if(tamanho == 0) throw "Pilha vazia!";
+    return topo->item;
+}
+
+int Pilha::GetTamanho(){
+    return tamanho;
+}
+
+void Pilha::Limpa(){
+    TipoCelula<int> *p;
+    while(topo != NULL){
+        p = topo;
+        topo = topo->prox;
+        delete p;
+    }
+    tamanho = 0;
+}
