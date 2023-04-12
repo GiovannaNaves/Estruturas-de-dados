d854f9a4708d81ef0f89fc15b124826577dc346a
GiovannaNaves <110492330+GiovannaNaves@users.noreply.github.com>
2023-04-11 23:50:18 -0300

    .

---
+++ Aula5/TP/src/pilha.cpp
@@ -0,0 +1,14 @@
+
+
+
+
+int main(){
+    Pilha p;
+    p.Empilhar(1);
+    p.Empilhar(2);
+    p.Empilhar(3);
+
+    cout << p.Desempilhar() << endl;
+    cout << p.Desempilhar() << endl;
+    cout << p.Desempilhar() << endl;
+}
