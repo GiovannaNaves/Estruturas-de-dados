2bd7fbdcab036f4b962c63ea435a7a823aec3f34
GiovannaNaves <110492330+GiovannaNaves@users.noreply.github.com>
2023-06-21 15:30:19 -0300

    .

---
+++ Aula11/TP/src/ConjuntoDisjunto.cpp
@@ -0,0 +1,9 @@
+#include "ConjuntoDisjunto.hpp" 
+
+	
+ConjuntoDisjunto::ConjuntoDisjunto(){}
+ConjuntoDisjunto::~ConjuntoDisjunto(){}
+
+void ConjuntoDisjunto::makeSet(long x){}
+void ConjuntoDisjunto::unionSet(long x, long y){}
+long ConjuntoDisjunto::findSet(long x){return 0;}
