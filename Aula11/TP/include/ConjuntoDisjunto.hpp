c95f3c99b7b1f37ee6f41afaffa691ac182c4101
GiovannaNaves <110492330+GiovannaNaves@users.noreply.github.com>
2023-06-26 14:16:37 -0300

    .

--- Aula11/TP/include/ConjuntoDisjunto.hpp
+++ Aula11/TP/include/ConjuntoDisjunto.hpp
@@ -1,7 +1,23 @@
+#define MAXN 101010
+
+struct No{
+	long valor;
+	// No* next;
+	No* pai;
+	int rank;
+	
+	No(long valor, int rank){
+		this->valor = valor;
+		// this->next = next;
+		this->pai = this;
+		this->rank = rank;
+	}
+};
 
 class ConjuntoDisjunto {
 private:
-	
+	int pai[MAXN];
+	No* conj[MAXN];
 
 public:	
 	ConjuntoDisjunto();
--- Aula11/TP/src/ConjuntoDisjunto.cpp
+++ Aula11/TP/src/ConjuntoDisjunto.cpp
@@ -1,9 +1,31 @@
 #include "ConjuntoDisjunto.hpp" 
+#include <iostream>
 
-	
 ConjuntoDisjunto::ConjuntoDisjunto(){}
 ConjuntoDisjunto::~ConjuntoDisjunto(){}
 
-void ConjuntoDisjunto::makeSet(long x){}
-void ConjuntoDisjunto::unionSet(long x, long y){}
-long ConjuntoDisjunto::findSet(long x){return 0;}
+void ConjuntoDisjunto::makeSet(long x){
+    conj[x] = new No(x, 0);
+}
+
+void ConjuntoDisjunto::unionSet(long x, long y){
+    No *xRoot = conj[findSet(x)];
+    No *yRoot = conj[findSet(y)];
+
+    if(xRoot->rank > yRoot->rank){
+        yRoot->pai = xRoot;
+    }else{
+        xRoot->pai = yRoot;
+        if(xRoot->rank == yRoot->rank){
+            yRoot->rank++;
+        }
+    }
+
+}
+long ConjuntoDisjunto::findSet(long x){
+    No* ptx = conj[x];
+    while(ptx->pai != ptx){
+        ptx = ptx->pai;
+    }
+    return ptx->valor;
+}
