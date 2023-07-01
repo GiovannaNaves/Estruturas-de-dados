3d9c0b5e53d04854ed8bfccfe5ac11587c0c393e
GiovannaNaves <110492330+GiovannaNaves@users.noreply.github.com>
2023-07-01 18:06:31 -0300

    .

--- TP3/include/huffman.hpp
+++ TP3/include/huffman.hpp
@@ -2,6 +2,9 @@
 #define HUFFMAN_H
 
 #include <iostream>
+#include <vector>
+#include <cstdio>
+#include <string>
 #include "queue.hpp"
 
 using namespace std;
@@ -47,7 +50,7 @@ class Huffman {
     }
 
     // traverse the Huffman Tree and decode the encoded string
-    void decode(Node* root, int& index, string str)
+    void decode(Node* root, int& index, string str, string& decodedStr)
     {
         if (root == nullptr) {
             return;
@@ -56,16 +59,50 @@ class Huffman {
         // found a leaf node
         if (!root->left && !root->right)
         {
-            cout << root->ch;
+            decodedStr += root->ch;
             return;
         }
 
         index++;
 
         if (str[index] == '0')
-            decode(root->left, index, str);
+            decode(root->left, index, str, decodedStr);
         else
-            decode(root->right, index, str);
+            decode(root->right, index, str, decodedStr);
+    }
+
+    void WriteBit(FILE* f, int bit, unsigned char& bit_buffer, int& current_bit){
+                if (bit)
+                    bit_buffer |= (1<<current_bit);
+                current_bit++;
+                        
+                if (current_bit == 8) {
+                    fwrite(&bit_buffer, 1, 1, f);
+                    bit_buffer = 0;
+                    current_bit = 0;
+                }
+
+    }            
+
+    void Flush_Bits(FILE* f, unsigned char& bit_buffer, int& current_bit) {
+                while (current_bit)
+                    WriteBit(f, 0, bit_buffer, current_bit);
+    }
+
+    void WriteHuffmanCode(const std::string& text, const std::string huffmanCode[], FILE* f) {
+                unsigned char bit_buffer = 0;
+                int current_bit = 0;
+
+                for (char ch : text) {
+                    string code = huffmanCode[ch];
+
+                    for (char c : code)
+                        WriteBit(f, 1, bit_buffer, current_bit);
+
+                    WriteBit(f, 0, bit_buffer, current_bit);
+                }
+
+                Flush_Bits(f, bit_buffer, current_bit);
     }
 
     // Builds Huffman Tree and decode given input text
@@ -124,34 +161,49 @@ class Huffman {
             string huffmanCode[MAX_CHAR];
             encode(root, "", huffmanCode);
 
-        /*
-            cout << "Huffman Codes are :\n" << '\n';
-            for (int i = 0; i < MAX_CHAR; i++) {
-                if (!huffmanCode[i].empty()) {
-                    cout << static_cast<char>(i) << " " << huffmanCode[i] << '\n';
-                }
+
+        if (action == 'c') {
+            // Create and open the binary file for writing
+            FILE* f = fopen("compressed.bin", "wb");
+            if (f == nullptr) {
+                cout << "Error opening file for writing." << endl;
+                return;
             }
-        */
-        if (action == 'c'){
-            // print encoded string
-            string encodedStr = "";
-            for (char ch : text) {
-                encodedStr += huffmanCode[ch];
-            }
-            cout << encodedStr << '\n';
+
+            // Encode the text and write Huffman code to the binary file
+            WriteHuffmanCode(text, huffmanCode, f);
+
+            // Close the file
+            fclose(f);
         }
        
         if (action == 'd'){
-            string encodedStr = "";
-            for (char ch : text) {
-                encodedStr += huffmanCode[ch];
+            std::string encodedStr;
+
+            // Open the binary file for reading
+            FILE* f = fopen("compressed.bin", "rb");
+            if (f == nullptr) {
+                cout << "Error opening file for reading." << endl;
+                return;
             }
-            // traverse the Huffman Tree again and this time
-            // decode the encoded string
+            // Read the binary file and store its content in encodedStr
+            fseek(f, 0, SEEK_END);
+            long fileSize = ftell(f);
+            fseek(f, 0, SEEK_SET);
+
+            encodedStr.resize(fileSize);
+            fread(&encodedStr[0], 1, fileSize, f);
+
+            // Close the file
+            fclose(f);
+
             int index = -1;
-            while (index < (int)encodedStr.size() - 2) {
-                decode(root, index, encodedStr);
+            string decodedStr;
+            
+            while (index < (int)encodedStr.size()) {
+                decode(root, index, encodedStr, decodedStr);
             }
+            cout << decodedStr << endl;
             // deallocate memory
             delete root;
         }
